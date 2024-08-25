467f724b11c43e7883600818049c76c0fdce26ea
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-08-24 21:13:04 -0300

    chore add migrations

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,11 @@
 import 'dotenv/config'
 import knex, { Knex } from 'knex'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found.z')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
